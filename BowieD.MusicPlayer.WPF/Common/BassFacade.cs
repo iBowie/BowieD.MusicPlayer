5999ca58bb5ed5ad588682638f52419d968f4eb5
BowieD <31174978+iBowie@users.noreply.github.com>
2021-12-10 20:42:46 +0500

    Update BassFacade.cs

--- BowieD.MusicPlayer.WPF/Common/BassFacade.cs
+++ BowieD.MusicPlayer.WPF/Common/BassFacade.cs
@@ -27,7 +27,7 @@ namespace BowieD.MusicPlayer.WPF.Common
 
         public static void Play(string fileName)
         {
-            if (State != BASSActive.BASS_ACTIVE_PAUSED)
+            if (State != BASSActive.BASS_ACTIVE_STOPPED)
             {
                 Stop();
 
