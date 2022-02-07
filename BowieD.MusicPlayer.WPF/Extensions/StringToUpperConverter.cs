9d533c125fada68d1f86ecb61c4c0460ebfe46cb
BowieD <31174978+iBowie@users.noreply.github.com>
2022-02-08 03:00:47 +0500

    use caps for visualizer

---
+++ BowieD.MusicPlayer.WPF/Extensions/StringToUpperConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BowieD.MusicPlayer.WPF.Extensions
+{
+    public sealed class StringToUpperConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+        {
+            return value?.ToString()?.ToUpper() ?? null;
+        }
+
+        public object? ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
