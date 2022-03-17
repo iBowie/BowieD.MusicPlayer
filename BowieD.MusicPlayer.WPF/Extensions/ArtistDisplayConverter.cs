6f63c7f82337f6518739dca654ea20998018dc0c
BowieD <31174978+iBowie@users.noreply.github.com>
2022-03-17 18:06:52 +0500

    display separated artists

--- BowieD.MusicPlayer.WPF/Extensions/ArtistDisplayConverter.cs
+++ BowieD.MusicPlayer.WPF/Extensions/ArtistDisplayConverter.cs
@@ -8,7 +8,17 @@ namespace BowieD.MusicPlayer.WPF.Extensions
     {
         public object? Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
         {
-            return value?.ToString()?.Replace(";", ", ").Replace("/", ", ") ?? null;
+            var res = value?.ToString()?
+                             .Replace(";", ", ")
+                             .Replace("/", ", ")
+                             ?? null;
+
+            if (parameter is IValueConverter subConverter)
+            {
+                return subConverter.Convert(res, targetType, null, culture);
+            }
+
+            return res;
         }
 
         public object? ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture)
