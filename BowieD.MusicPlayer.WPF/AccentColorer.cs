5fd3e5baa7f450c0e8d9e0eed2cbfde610de26fd
BowieD <31174978+iBowie@users.noreply.github.com>
2022-03-17 16:40:23 +0500

    Update AccentColorer.cs

--- BowieD.MusicPlayer.WPF/AccentColorer.cs
+++ BowieD.MusicPlayer.WPF/AccentColorer.cs
@@ -33,11 +33,39 @@ namespace BowieD.MusicPlayer.WPF
 
             App.Current.Resources["MahApps.Brushes.AccentBase"] = accentBase.brush;
             App.Current.Resources["MahApps.Brushes.Accent"] = accent.brush;
-            App.Current.Resources["MahApps.Brushes.WindowTitle"] = accent.brush;
             App.Current.Resources["MahApps.Brushes.Accent2"] = accent2.brush;
             App.Current.Resources["MahApps.Brushes.Accent3"] = accent3.brush;
             App.Current.Resources["MahApps.Brushes.Accent4"] = accent4.brush;
             App.Current.Resources["MahApps.Brushes.Highlight"] = highlight.brush;
+
+            App.Current.Resources["MahApps.Brushes.WindowTitle"] = accent.brush;
+            App.Current.Resources["MahApps.Brushes.TextBlock.FloatingMessage"] = accentBase.brush;
+            App.Current.Resources["MahApps.Brushes.Badged.Background"] = accentBase.brush;
+            App.Current.Resources["MahApps.Brushes.Dialog.Background.Accent"] = highlight.brush;
+            App.Current.Resources["MahApps.Brushes.Dialog.Glow"] = accent.brush;
+            App.Current.Resources["MahApps.Brushes.CheckmarkFill"] = accent.brush;
+            App.Current.Resources["MahApps.Brushes.RightArrowFill"] = accent.brush;
+
+            App.Current.Resources["MahApps.Brushes.DataGrid.Selection.Background"] = accent.brush;
+            App.Current.Resources["MahApps.Brushes.DataGrid.Selection.Background.Inactive"] = accent3.brush;
+            App.Current.Resources["MahApps.Brushes.DataGrid.Selection.Background.MouseOver"] = accent2.brush;
+            App.Current.Resources["MahApps.Brushes.DataGrid.Selection.BorderBrush"] = accent.brush;
+            App.Current.Resources["MahApps.Brushes.DataGrid.Selection.BorderBrush.Focus"] = accent.brush;
+            App.Current.Resources["MahApps.Brushes.DataGrid.Selection.BorderBrush.Inactive"] = accent3.brush;
+            App.Current.Resources["MahApps.Brushes.DataGrid.Selection.BorderBrush.MouseOver"] = accent2.brush;
+
+            LinearGradientBrush lgb = new()
+            {
+                StartPoint = new(1.002,0.5),
+                EndPoint = new(0.001, 0.5),
+            };
+
+            lgb.GradientStops.Add(new(highlight.color, 0));
+            lgb.GradientStops.Add(new(accent3.color, 1));
+
+            lgb.Freeze();
+
+            App.Current.Resources["MahApps.Brushes.Progress"] = lgb;
         }
     }
 }
